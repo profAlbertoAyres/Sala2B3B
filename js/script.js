7b9677b89c0a20fb3160507649814cadbfb77e54
profAlbertoAyres <alberto.benicio@ifro.edu.br>
2023-05-03 11:54:10 -0400

    Aula 03/05

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function abrirMenu(){
+    const divMenu = document.getElementById('respMenu').classList;
+    divMenu.toggle('abrir');
+    const ic = document.getElementById('icone');
+    if(divMenu.contains('abrir')){
+        ic.innerHTML = 'close';
+    }else{
+        ic.innerHTML = 'menu';
+    }
+}
